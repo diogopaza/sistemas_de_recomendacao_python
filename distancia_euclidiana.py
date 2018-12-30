9cbd72d2f6338a2aa90ada7c8d245ad18b67c131
Diogo Paza <diogopazacvel@gmail.com>
2018-12-30 16:30:25 -0200

    distancia_euclidiana

--- distancia_euclidiana.py
+++ distancia_euclidiana.py
@@ -1,6 +1,14 @@
 from recomendacao import avaliacoes
+from math import sqrt
 
-nota_marcos_exterminador = avaliacoes['Marcos']['Exterminador do Futuro']
-print( pow(5,2) )
+#nota_marcos_exterminador = avaliacoes['Marcos']['Exterminador do Futuro']
+#print( pow(5,2) )
+#print( nota_marcos_exterminador )
+"""
+print( pow( 3 - 3,2 ) )
+print( pow( 3.5 - 4,2 ) )
 
-print( nota_marcos_exterminador )
+print( sqrt( pow( 3 - 3,2 ) + pow( 3.5 - 4,2 ) ) )
+"""
+
+print (1/ ( 1+ sqrt( pow( 3 - 1.5,2 ) + pow( 3.5 - 5,2 ) ) ) )
